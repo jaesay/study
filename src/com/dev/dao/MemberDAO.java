723b1cb55535777d1fa2b13ffec3095f05967d83
jaeseong <jaeseong@LAPTOP-C57O7Q05>
2018-07-25 22:49:59 +0900

    Create Insert

---
+++ src/com/dev/dao/MemberDAO.java
@@ -0,0 +1,74 @@
+package com.dev.dao;
+
+import java.sql.*;
+import com.dev.vo.MemberVO;
+
+public class MemberDAO {
+	
+	private static MemberDAO dao = new MemberDAO();
+	
+	private MemberDAO() {}
+	
+	public static MemberDAO getInstance() {
+		return dao;
+	}
+	
+	public Connection connect() {
+		Connection conn = null;
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "scott", "tiger");
+		} catch (Exception e) {
+			System.out.println("오류 발생 : " + e); 
+		}
+		return conn;
+	}
+	
+	public void close(Connection conn, PreparedStatement ps, ResultSet rs) {
+		if(rs != null) {
+			try {
+				rs.close();
+			} catch (Exception e) {
+				System.out.println("오류 발생 : " + e);
+			}
+		}
+		close(conn, ps);
+	}
+	
+	public void close(Connection conn, PreparedStatement ps) {
+		if(ps != null) {
+			try {
+				ps.close();
+			} catch (Exception e) {
+				System.out.println("오류 발생 : " + e);
+			}
+		}
+		if(conn != null) {
+			try {
+				conn.close();
+			} catch (Exception e) {
+				System.out.println("오류 발생 : " + e);
+			}
+		}
+	}
+	
+	public void memberInsert(MemberVO member) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			conn = connect();
+			pstmt = conn.prepareStatement("inser into member values(?,?,?,?)");
+			pstmt.setString(1, member.getId());
+			pstmt.setString(2, member.getPasswd());
+			pstmt.setString(3, member.getName());
+			pstmt.setString(4, member.getMail());
+			pstmt.executeUpdate();
+		} catch (Exception e) {
+			System.out.println("오류 발생 : " + e);
+		} finally {
+			close(conn, pstmt);
+		}
+	}
+	
+}
