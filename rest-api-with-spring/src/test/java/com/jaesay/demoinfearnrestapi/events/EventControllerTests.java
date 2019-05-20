9350c4097f1b979b63fe51f65f40974603ba8ce2
jaesay <jaesay@ajou.ac.kr>
2019-05-20 22:00:04 +0900

    Add create events test

---
+++ rest-api-with-spring/src/test/java/com/jaesay/demoinfearnrestapi/events/EventControllerTests.java
@@ -0,0 +1,31 @@
+package com.jaesay.demoinfearnrestapi.events;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class EventControllerTests {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void createEvent() throws Exception {
+        mockMvc.perform(post("/api/events/")
+                    .contentType(MediaType.APPLICATION_JSON_UTF8) // 이 요청의 본문에 json을 담아서 보내고 있다.
+                    .accept(MediaTypes.HAL_JSON)    // HAL_JSON 스펙에 준하는 응답을 받고 싶다.
+                )
+                .andExpect(status().isCreated());
+    }
+}
