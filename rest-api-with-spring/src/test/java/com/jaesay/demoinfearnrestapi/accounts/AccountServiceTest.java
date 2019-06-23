49e51b5d37a35841d214777c8f177c16bab63e22
jaesay <jaesay@ajou.ac.kr>
2019-06-23 19:17:04 +0900

    5.REST API 보안 적용 - 스프링 시큐리티 적용

---
+++ rest-api-with-spring/src/test/java/com/jaesay/demoinfearnrestapi/accounts/AccountServiceTest.java
@@ -0,0 +1,48 @@
+package com.jaesay.demoinfearnrestapi.accounts;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class AccountServiceTest {
+
+    @Autowired
+    AccountService accountService;
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    @Test
+    public void findByUsername() {
+        // Given
+        String username = "keesun@email.com";
+        String password = "keesun";
+        Account account = Account.builder()
+                .email(username)
+                .password(password)
+                .roles(Set.of(AccountRole.ADMIN, AccountRole.USER))
+                .build();
+
+        this.accountRepository.save(account);
+
+        // When
+        UserDetailsService userDetailsService = (UserDetailsService) accountService;
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+        // Then
+        assertThat(userDetails.getPassword()).isEqualTo(password);
+    }
+
+}
