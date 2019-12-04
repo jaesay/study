9f7e2e5b818fd1db0ffc523332f4124ed90da474
jaesay <jaesay@ajou.ac.kr>
2019-12-04 15:53:22 +0900

    프로젝트 생성

---
+++ jojoldubook/src/main/java/com/study/jojoldubook/JojoldubookApplication.java
@@ -0,0 +1,13 @@
+package com.study.jojoldubook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JojoldubookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JojoldubookApplication.class, args);
+    }
+
+}
---
+++ jojoldubook/src/test/java/com/study/jojoldubook/JojoldubookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.jojoldubook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JojoldubookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
