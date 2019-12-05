0b1602dd77fb386f2ccec49e7287fc938524f853
jaesay <jaesay@ajou.ac.kr>
2019-12-05 17:31:02 +0900

    Chapter03까지

--- jojoldubook/src/main/java/com/study/jojoldubook/Application.java
+++ jojoldubook/src/main/java/com/study/jojoldubook/Application.java
@@ -2,12 +2,14 @@ package com.study.jojoldubook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
-public class JojoldubookApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(JojoldubookApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
---
+++ jojoldubook/src/main/java/com/study/jojoldubook/web/PostsApiController.java
@@ -0,0 +1,30 @@
+package com.study.jojoldubook.web;
+
+import com.study.jojoldubook.service.posts.PostsService;
+import com.study.jojoldubook.web.dto.PostsResponseDto;
+import com.study.jojoldubook.web.dto.PostsSaveRequestDto;
+import com.study.jojoldubook.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+}
