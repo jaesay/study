34183b18b9674aa03472df44634e3f57966eaf01
jaesay <jaesay@ajou.ac.kr>
2019-12-06 13:44:59 +0900

    Chapter04까지

--- jojoldubook/src/main/java/com/study/jojoldubook/service/posts/PostsService.java
+++ jojoldubook/src/main/java/com/study/jojoldubook/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.study.jojoldubook.service.posts;
 
 import com.study.jojoldubook.domain.posts.Posts;
 import com.study.jojoldubook.domain.posts.PostsRepository;
+import com.study.jojoldubook.web.dto.PostsListResponseDto;
 import com.study.jojoldubook.web.dto.PostsResponseDto;
 import com.study.jojoldubook.web.dto.PostsSaveRequestDto;
 import com.study.jojoldubook.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -29,10 +33,26 @@ public class PostsService {
         return id;
     }
 
+    @Transactional(readOnly = true)
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
     }
 }
