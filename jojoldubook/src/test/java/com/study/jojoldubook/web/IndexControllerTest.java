14469e21d5f51a2e9a073ce32a5a4c65c28766c2
jaesay <jaesay@ajou.ac.kr>
2019-12-09 17:10:56 +0900

    .gradlew test에서 out of memory 해결 테스트1

--- jojoldubook/src/test/java/com/study/jojoldubook/web/IndexControllerTest.java
+++ jojoldubook/src/test/java/com/study/jojoldubook/web/IndexControllerTest.java
@@ -14,15 +14,15 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 public class IndexControllerTest {
 
-    /*@Autowired
-    private TestRestTemplate restTemplate;
+//    @Autowired
+//    private TestRestTemplate restTemplate;
 
     @Test
     public void 메인페이지_로딩() {
-        //when
-        String body = this.restTemplate.getForObject("/", String.class);
-
-        //then
-        assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
-    }*/
+//        //when
+//        String body = this.restTemplate.getForObject("/", String.class);
+//
+//        //then
+//        assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
+    }
 }
