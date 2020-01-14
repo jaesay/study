d58c5863804d07b7389bd7f7368b6da047897b3d
jaesay <jaesay@ajou.ac.kr>
2020-01-14 21:04:58 +0900

    테스트 코드 오류 수정

--- web/src/main/java/toyproject/ecommerce/web/WebApplication.java
+++ web/src/main/java/toyproject/ecommerce/web/WebApplication.java
@@ -2,8 +2,13 @@ package toyproject.ecommerce.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = {"toyproject.ecommerce.web","toyproject.ecommerce.core"})
+//@SpringBootApplication(scanBasePackages = {"toyproject.ecommerce.web","toyproject.ecommerce.core"})
+@SpringBootApplication
+@EntityScan("toyproject.ecommerce.*")
+@EnableJpaRepositories("toyproject.ecommerce.*")
 public class WebApplication {
 
     public static void main(String[] args) {
