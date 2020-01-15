843c178c3ecf79ce3e2633bd79c630547853eeb1
jaesay <jaesay@ajou.ac.kr>
2020-01-15 19:59:58 +0900

    등록/수정 시간 JPA Auditing

--- web/src/main/java/toyproject/ecommerce/web/WebApplication.java
+++ web/src/main/java/toyproject/ecommerce/web/WebApplication.java
@@ -2,11 +2,9 @@ package toyproject.ecommerce.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication(scanBasePackages = {"toyproject.ecommerce.web","toyproject.ecommerce.core"})
-//@SpringBootApplication
-/*@EntityScan("toyproject.ecommerce.*")
-@EnableJpaRepositories("toyproject.ecommerce.*")*/
 public class WebApplication {
 
     public static void main(String[] args) {
