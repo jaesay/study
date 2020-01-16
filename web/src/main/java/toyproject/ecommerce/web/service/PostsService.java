1eae82d3d7978d9885325c1fa96902acbcc60617
jaesay <jaesay@ajou.ac.kr>
2020-01-16 20:53:29 +0900

    게시글 등록

--- web/src/main/java/toyproject/ecommerce/web/service/PostsService.java
+++ web/src/main/java/toyproject/ecommerce/web/service/PostsService.java
@@ -5,9 +5,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import toyproject.ecommerce.core.domain.posts.Posts;
 import toyproject.ecommerce.core.domain.posts.PostsRepository;
+import toyproject.ecommerce.web.controller.dto.PostsListResponseDto;
 import toyproject.ecommerce.web.controller.dto.PostsResponseDto;
 import toyproject.ecommerce.web.controller.dto.PostsSaveRequestDto;
 import toyproject.ecommerce.web.controller.dto.PostsUpdateRequestDto;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -31,5 +35,12 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
