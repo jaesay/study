71dacb4971c730e4b0e07a1b2d5735225171debf
jaesay <jaesay@ajou.ac.kr>
2020-01-23 20:59:27 +0900

    web 배포 deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+!/bin/bash
 
 REPOSITORY=/home/ec2-user/app/step1
 PROJECT_NAME=toyproject-ecommerce
@@ -13,13 +13,13 @@ echo "> 프로젝트 Build 시작"
 
 ./gradlew build
 
-echo "> step1 디렉토리 이동"
+echo "> step1 디렉토리로 이동"
 
 cd $REPOSITORY
 
-echo "> Build 파일복사"
+echo "> Build 파일 복사"
 
-cp $REPOSITORY/$PROJECT_NAME/build/libs/*.jar $REPOSITORY/
+cp $REPOSITORY/$PROJECT_NAME/web/build/libs/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
@@ -28,11 +28,11 @@ CURRENT_PID=$(pgrep -f ${PROJECT_NAME}*.jar)
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
-  echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+        echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-  echo "> kill -15 $CURRENT_PID"
-  kill -15 $CURRENT_PID
-  sleep 5
+        echo "> kill -15 $CURRENT_PID"
+        kill -15 $CURRENT_PID
+        sleep 5
 fi
 
 echo "> 새 애플리케이션 배포"
