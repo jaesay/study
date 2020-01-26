87c317c8e37f0363922a637198d0e9b04e9876f8
jaesay <jaesay@ajou.ac.kr>
2020-01-26 10:01:54 +0900

    자동 배포 테스트2

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,7 +10,7 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl jojoldubook | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl web | grep jar | awk '{print $1}')
 
 echo "> 현재 구동 중인 애플리케이션 pid $CURRENT_PID"
 
