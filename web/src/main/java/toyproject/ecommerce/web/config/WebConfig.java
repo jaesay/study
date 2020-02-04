dae40c4315839aec0c08ed894bb13c5d0e638260
jaesay <jaesay@ajou.ac.kr>
2020-02-04 19:55:53 +0900

    서비스 시작 시 기본 계정 생성

--- web/src/main/java/toyproject/ecommerce/web/config/WebConfig.java
+++ web/src/main/java/toyproject/ecommerce/web/config/WebConfig.java
@@ -2,13 +2,20 @@ package toyproject.ecommerce.web.config;
 
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import toyproject.ecommerce.core.domain.Member;
+import toyproject.ecommerce.core.domain.enums.Role;
+import toyproject.ecommerce.core.repository.MemberRepository;
 import toyproject.ecommerce.web.config.oauth.LoginUserArgumentResolver;
+import toyproject.ecommerce.web.service.MemberService;
 
 import java.util.List;
 
@@ -30,5 +37,30 @@ public class WebConfig implements WebMvcConfigurer {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public ApplicationRunner applicationRunner() {
+        return new ApplicationRunner() {
+            @Autowired MemberRepository memberRepository;
+            @Autowired PasswordEncoder passwordEncoder;
+
+            @Override
+            public void run(ApplicationArguments args) throws Exception {
+                String email = "member1@test.com";
+
+                Member member1 = memberRepository.findByEmail(email)
+                        .orElse(Member.builder()
+                                .email(email)
+                                .password(passwordEncoder.encode("1111"))
+                                .name("member1")
+                                .role(Role.USER)
+                                .build());
+
+                if (member1.getId() == null) {
+                   memberRepository.save(member1);
+                }
+            }
+        };
     }
 }
