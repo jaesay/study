7626f805622060a4c8604d208bf7a8c3f6a21fbd
jaesay <jaesay@ajou.ac.kr>
2020-02-11 18:57:39 +0900

    Search

--- web/src/main/java/toyproject/ecommerce/web/controller/IndexController.java
+++ web/src/main/java/toyproject/ecommerce/web/controller/IndexController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import toyproject.ecommerce.core.domain.Cart;
 import toyproject.ecommerce.core.domain.dto.ItemSearch;
 import toyproject.ecommerce.core.domain.dto.ItemSummaryDto;
