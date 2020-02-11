f20c5f832f80fc21eb3a202c371c15b1e195b679
jaesay <jaesay@ajou.ac.kr>
2020-02-12 00:40:54 +0900

    Admin item list

--- admin/src/main/java/toyproject/ecommerce/admin/controller/ItemController.java
+++ admin/src/main/java/toyproject/ecommerce/admin/controller/ItemController.java
@@ -1,9 +1,14 @@
 package toyproject.ecommerce.admin.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import toyproject.ecommerce.admin.service.ItemService;
+import toyproject.ecommerce.core.domain.Item;
 
 @RequiredArgsConstructor
 @Controller
@@ -12,7 +17,11 @@ public class ItemController {
     private final ItemService itemService;
 
     @GetMapping("/items")
-    public String itemList() {
+    public String itemList(@PageableDefault(value = 9) Pageable pageable, Model model) {
+
+        Page<Item> items = itemService.findItems(pageable);
+        model.addAttribute("items", items);
+
         return "itemList";
     }
 }
--- admin/src/main/java/toyproject/ecommerce/admin/service/ItemService.java
+++ admin/src/main/java/toyproject/ecommerce/admin/service/ItemService.java
@@ -1,9 +1,25 @@
 package toyproject.ecommerce.admin.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import toyproject.ecommerce.core.domain.Item;
+import toyproject.ecommerce.core.repository.ItemRepository;
 
 @RequiredArgsConstructor
 @Service
 public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    public Page<Item> findItems(Pageable pageable) {
+        pageable = PageRequest.of(pageable.getPageNumber() <= 0 ? 0 : pageable.getPageNumber() - 1,
+                pageable.getPageSize(),
+                Sort.by("id").descending());
+
+        return itemRepository.findAll(pageable);
+    }
 }
