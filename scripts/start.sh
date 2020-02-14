a266341e893c8f71c8df274582428f0ef8946e45
jaesay <jaesay@ajou.ac.kr>
2020-02-14 23:20:58 +0900

    Web module start script rollback

--- scripts/start.sh
+++ scripts/start.sh
@@ -26,7 +26,7 @@ echo "> $JAR_NAME 실행"
 IDLE_PROFILE=$(find_idle_profile)
 
 echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
-nohup java -XX:MaxMetaspaceSize=256m -XX:MetaspaceSize=128m -jar \
+nohup java -jar \
     -Dspring.config.location=classpath:/application.yml,classpath:/application-$IDLE_PROFILE.yml,/home/ec2-user/app/application-oauth.yml,/home/ec2-user/app/application-real-db.yml\
     -Dspring.profiles.active=$IDLE_PROFILE \
     $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
