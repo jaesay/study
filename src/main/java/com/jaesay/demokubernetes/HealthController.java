223d3d659d02df78ceaea06477f8313857eeee06
jaesay <jaesay@ajou.ac.kr>
2020-11-22 14:34:57 +0900

    Add health api

---
+++ src/main/java/com/jaesay/demokubernetes/HealthController.java
@@ -0,0 +1,13 @@
+package com.jaesay.demokubernetes;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "health check";
+    }
+}
