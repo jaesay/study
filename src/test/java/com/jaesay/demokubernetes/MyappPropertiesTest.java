0cf0658f3137b04809c009a691a8aeb9378aa573
jaesay <jaesay@ajou.ac.kr>
2020-11-29 18:40:28 +0900

    Add github actions

---
+++ src/test/java/com/jaesay/demokubernetes/MyappPropertiesTest.java
@@ -0,0 +1,19 @@
+package com.jaesay.demokubernetes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class MyappPropertiesTest {
+
+    @Autowired
+    private MyappProperties myappProperties;
+
+    @Test
+    void getName_shouldSucceed() {
+        assertEquals("test\ntest", myappProperties.getName());
+    }
+}
