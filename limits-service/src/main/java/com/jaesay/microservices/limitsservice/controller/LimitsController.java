5ad5b1388821e18c5d227edec1db47f4fdc57c46
jaesay <jaesay@ajou.ac.kr>
2021-03-30 20:32:07 +0900

    Enhance limits service

--- limits-service/src/main/java/com/jaesay/microservices/limitsservice/controller/LimitsController.java
+++ limits-service/src/main/java/com/jaesay/microservices/limitsservice/controller/LimitsController.java
@@ -1,17 +1,22 @@
 package com.jaesay.microservices.limitsservice.controller;
 
 import com.jaesay.microservices.limitsservice.bean.Limits;
+import com.jaesay.microservices.limitsservice.configuration.Configuration;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class LimitsController {
+
+    private final Configuration configuration;
 
     @GetMapping("/limits")
     public Limits retrieveLimits() {
         return Limits.builder()
-                .minimum(1)
-                .maximum(1000)
+                .minimum(configuration.getMinimum())
+                .maximum(configuration.getMaximum())
                 .build();
     }
 }
