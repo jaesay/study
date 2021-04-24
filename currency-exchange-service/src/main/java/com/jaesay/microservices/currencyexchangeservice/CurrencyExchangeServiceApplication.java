695f94b0b8781e718bbcae1388968d56fc956d00
jaesay <jaesay@ajou.ac.kr>
2021-04-24 23:35:02 +0900

    Add naming server

--- currency-exchange-service/src/main/java/com/jaesay/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
+++ currency-exchange-service/src/main/java/com/jaesay/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -1,7 +1,12 @@
 package com.jaesay.microservices.currencyexchangeservice;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @SpringBootApplication
 public class CurrencyExchangeServiceApplication {
@@ -10,4 +15,28 @@ public class CurrencyExchangeServiceApplication {
         SpringApplication.run(CurrencyExchangeServiceApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner runner(CurrencyExchangeRepository repository) {
+        return args -> {
+            List<CurrencyExchange> currencyExchanges = List.of(
+                    CurrencyExchange.builder()
+                            .from("USD")
+                            .to("KRW")
+                            .conversionMultiple(BigDecimal.valueOf(10))
+                            .build(),
+                    CurrencyExchange.builder()
+                            .from("EUR")
+                            .to("KRW")
+                            .conversionMultiple(BigDecimal.valueOf(20))
+                            .build(),
+                    CurrencyExchange.builder()
+                            .from("AUD")
+                            .to("KRW")
+                            .conversionMultiple(BigDecimal.valueOf(30))
+                            .build()
+            );
+
+            repository.saveAll(currencyExchanges);
+        };
+    }
 }
