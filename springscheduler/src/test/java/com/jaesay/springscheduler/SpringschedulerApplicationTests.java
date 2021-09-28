8c085339d9d6df875eadab1b18cb6093f77b69d0
Kim Jae Seong <jaesay@ajou.ac.kr>
2021-09-28 22:16:33 +0900

    Add springscheduler

---
+++ springscheduler/src/test/java/com/jaesay/springscheduler/SpringschedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaesay.springscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringschedulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
